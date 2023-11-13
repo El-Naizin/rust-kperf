d390fb5f2b7fa2cab47fff36cb7a6831e9d1db03
Leo Naizin <lnaizin@gmail.com>
2023-11-13 22:09:48 +0100

    database creation

--- kperf-tests/src/main.rs
+++ kperf-tests/src/main.rs
@@ -1,15 +1,29 @@
+use std::ffi::CStr;
+use std::ptr::{null, null_mut};
 use libc::c_int;
 use kperf_sys;
+use kperf_sys::structs::kpep_db;
 
 
 fn main() {
     let mut force_ctrs: c_int = 0;
     unsafe {
+        // Check permission
         if kperf_sys::functions::kpc_force_all_ctrs_get(&mut force_ctrs) != 0{
             println!("Permission denied, xnu/kpc requires root privileges");
             return;
         }
         println!("{}", force_ctrs);
+
+        // Load pmc database
+        let mut db: *mut kpep_db = null_mut();
+        let mut ret = kperf_sys::functions::kpep_db_create(
+            null(),
+            &mut db,
+        );
+        let db_name = CStr::from_ptr((*db).name);
+        let db_marketing_name = CStr::from_ptr((*db).marketing_name);
+        println!("loaded db: {:?} ({:?})", db_name, db_marketing_name);
     }
     println!("Hello, world!");
 }
