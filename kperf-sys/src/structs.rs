0eec123969be399afdf17da1577be0351dac5d40
Leo Naizin <lnaizin@gmail.com>
2023-11-13 22:09:23 +0100

    made framework struct fields public

--- kperf-sys/src/structs.rs
+++ kperf-sys/src/structs.rs
@@ -7,59 +7,59 @@ pub type kpc_config_t = u64;
 #[repr(C)]
 #[allow(non_camel_case_types)]
 pub struct kpep_event {
-    name: *const c_char, ///< Unique name of a event, such as "INST_RETIRED.ANY".
-    description: *const c_char, ///< Description for this event.
-    errata: *const c_char,      ///< Errata, currently NULL.
-    alias: *const c_char,       ///< Alias name, such as "Instructions", "Cycles".
-    fallback: *const c_char,    ///< Fallback event name for fixed counter.
-    mask: c_uint,
-    number: c_uchar,
-    umask: c_uchar,
-    reserved: c_uchar,
-    is_fixed: c_uchar,
+    pub name: *const c_char, ///< Unique name of a event, such as "INST_RETIRED.ANY".
+    pub description: *const c_char, ///< Description for this event.
+    pub errata: *const c_char,      ///< Errata, currently NULL.
+    pub alias: *const c_char,       ///< Alias name, such as "Instructions", "Cycles".
+    pub fallback: *const c_char,    ///< Fallback event name for fixed counter.
+    pub mask: c_uint,
+    pub number: c_uchar,
+    pub umask: c_uchar,
+    pub reserved: c_uchar,
+    pub is_fixed: c_uchar,
 }
 
 /// KPEP database (size: 144/80 bytes on 64/32 bit OS)
 #[allow(non_camel_case_types)]
 #[repr(C)]
 pub struct kpep_db {
-    name: *const c_char,           ///< Database name, such as "haswell".
-    cpu_id: *const c_char,         ///< Plist name, such as "cpu_7_8_10b282dc".
-    marketing_name: *const c_char, ///< Marketing name, such as "Intel Haswell".
-    plist_data: *mut c_void,           ///< Plist data (CFDataRef), currently NULL.
-    event_map: *mut c_void, ///< All events (CFDict<CFSTR(event_name), kpep_event *>).
-    event_arr: *mut kpep_event, ///< Event struct buffer (sizeof(kpep_event) * events_count).
-    fixed_event_arr: *mut *mut kpep_event, ///< Fixed counter events (sizeof(kpep_event *)
+    pub name: *const c_char,           ///< Database name, such as "haswell".
+    pub cpu_id: *const c_char,         ///< Plist name, such as "cpu_7_8_10b282dc".
+    pub marketing_name: *const c_char, ///< Marketing name, such as "Intel Haswell".
+    pub plist_data: *mut c_void,           ///< Plist data (CFDataRef), currently NULL.
+    pub event_map: *mut c_void, ///< All events (CFDict<CFSTR(event_name), kpep_event *>).
+    pub event_arr: *mut kpep_event, ///< Event struct buffer (sizeof(kpep_event) * events_count).
+    pub fixed_event_arr: *mut *mut kpep_event, ///< Fixed counter events (sizeof(kpep_event *)
     ///< * fixed_counter_count)
-    alias_map: *mut c_void, ///< All aliases (CFDict<CFSTR(event_name), kpep_event *>).
-    reserved_1: size_t,
-    reserved_2: size_t,
-    reserved_3: size_t,
-    event_count: size_t, ///< All events count.
-    alias_count: size_t,
-    fixed_counter_count: size_t,
-    config_counter_count: size_t,
-    power_counter_count: size_t,
-    archtecture: c_uint, ///< see `KPEP CPU archtecture constants` above.
-    fixed_counter_bits: c_uint,
-    config_counter_bits: c_uint,
-    power_counter_bits: c_uint,
+    pub alias_map: *mut c_void, ///< All aliases (CFDict<CFSTR(event_name), kpep_event *>).
+    pub reserved_1: size_t,
+    pub reserved_2: size_t,
+    pub reserved_3: size_t,
+    pub event_count: size_t, ///< All events count.
+    pub alias_count: size_t,
+    pub fixed_counter_count: size_t,
+    pub config_counter_count: size_t,
+    pub power_counter_count: size_t,
+    pub archtecture: c_uint, ///< see `KPEP CPU archtecture constants` above.
+    pub fixed_counter_bits: c_uint,
+    pub config_counter_bits: c_uint,
+    pub power_counter_bits: c_uint,
 }
 
 /// KPEP config (size: 80/44 bytes on 64/32 bit OS)
 #[repr(C)]
 #[allow(non_camel_case_types)]
 pub struct kpep_config {
-    db: *mut kpep_db,
-    ev_arr: *mut *mut kpep_event, ///< (sizeof(kpep_event *) * counter_count), init NULL
-    ev_map: *mut size_t,       ///< (sizeof(size_t *) * counter_count), init 0
-    ev_idx: *mut size_t,       ///< (sizeof(size_t *) * counter_count), init -1
-    flags: *mut c_uint,          ///< (sizeof(c_uint *) * counter_count), init 0
-    kpc_periods: *mut c_ulonglong,    ///< (sizeof(c_ulonglong *) * counter_count), init 0
-    event_count: size_t,   /// kpep_config_events_count()
-    counter_count: size_t,
-    classes: c_uint, ///< See `class mask constants` above.
-    config_counter: c_uint,
-    power_counter: c_uint,
-    reserved: c_uint,
+    pub db: *mut kpep_db,
+    pub ev_arr: *mut *mut kpep_event, ///< (sizeof(kpep_event *) * counter_count), init NULL
+    pub ev_map: *mut size_t,       ///< (sizeof(size_t *) * counter_count), init 0
+    pub ev_idx: *mut size_t,       ///< (sizeof(size_t *) * counter_count), init -1
+    pub flags: *mut c_uint,          ///< (sizeof(c_uint *) * counter_count), init 0
+    pub kpc_periods: *mut c_ulonglong,    ///< (sizeof(c_ulonglong *) * counter_count), init 0
+    pub event_count: size_t,   /// kpep_config_events_count()
+    pub counter_count: size_t,
+    pub classes: c_uint, ///< See `class mask constants` above.
+    pub config_counter: c_uint,
+    pub power_counter: c_uint,
+    pub reserved: c_uint,
 }
