e9bf3a97487501e67d0e154c7aeeaafd76142bc5
Leo Naizin <lnaizin@gmail.com>
2023-11-13 21:17:45 +0100

    Started library tests

--- kperf-tests/src/main.rs
+++ kperf-tests/src/main.rs
@@ -1,3 +1,15 @@
+use libc::c_int;
+use kperf_sys;
+
+
 fn main() {
+    let mut force_ctrs: c_int = 0;
+    unsafe {
+        if kperf_sys::functions::kpc_force_all_ctrs_get(&mut force_ctrs) != 0{
+            println!("Permission denied, xnu/kpc requires root privileges");
+            return;
+        }
+        println!("{}", force_ctrs);
+    }
     println!("Hello, world!");
 }
