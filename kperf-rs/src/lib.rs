1b2b6a9bac7a64adf76b0f6d417ed8d14d522b0b
Leo Naizin <lnaizin@gmail.com>
2024-01-01 21:53:50 +0100

    Safe wrapper for initialisation of Kpep DB and Kpep Config

--- kperf-rs/src/lib.rs
+++ kperf-rs/src/lib.rs
@@ -1,14 +1,158 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub use kperf_sys;
+use libc::c_int;
+use std::error::Error;
+use std::ffi::CString;
+use std::io::ErrorKind::PermissionDenied;
+use std::ptr::{null, null_mut};
+use kperf_sys::functions::{kpep_config_create, kpep_db_event};
+use kperf_sys::structs::{kpep_config, kpep_db, kpep_event};
+
+#[derive(Debug, Copy, Clone)]
+pub enum KpepError {
+    UnknownError
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug)]
+pub struct KProbesConfig {
+    pub config: *mut kpep_config,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl KProbesConfig {
+    pub fn from_database(database: &mut KProbesDatabase) -> Result<Self, KpepError> {
+        let mut config = null_mut();
+        unsafe {
+            let res = kpep_config_create(database.database, &mut config);
+            if res != 0 {
+                // println!(
+                //     "Failed to create kpep config, error: {}",
+                //     res
+                // );
+                return Err(KpepError::UnknownError)
+            }
+        }
+
+        return Ok(Self {
+            config
+        })
     }
+}
+
+#[derive(Debug)]
+pub struct KProbesDatabase {
+    pub database: *mut kpep_db,
+}
+
+impl KProbesDatabase {
+    pub fn load_database() -> Result<Self, KpepError> {
+        let mut db: *mut kpep_db = null_mut();
+        unsafe {
+            let mut ret = kperf_sys::functions::kpep_db_create(
+                null(),
+                &mut db,
+            );
+            if ret != 0 {
+                return Err(KpepError::UnknownError)
+            }
+        }
+
+        Ok(Self {
+            database: db
+        })
+    }
+}
+
+pub struct PerfCounterBuilder {
+}
+
+impl PerfCounterBuilder {
+    fn build_counter() {
+    }
+}
+
+pub struct PerfCounter {
+}
+
+impl PerfCounter {
+    fn start(&mut self) {
+    }
+
+    fn stop(&mut self) {
+    }
+
+    fn reset(&mut self) {
+    }
+
+    fn read(&mut self) {
+    }
+}
+
+#[derive(Debug)]
+pub enum KperfError {
+    PermissionDenied,
+}
+
+#[derive(Copy, Clone, Debug)]
+pub enum Event {
+    Cycles,
+    Instructions,
+    Branches,
+    BranchMisses,
+}
+
+pub fn get_event_names(event_type: Event) -> Vec<CString> {
+    match event_type {
+        Event::Cycles => {
+            vec![
+                CString::new("FIXED_CYCLES").unwrap(),             // Apple A7-A15
+                CString::new("CPU_CLK_UNHALTED.THREAD").unwrap(),  // Intel Core 1th-10th
+                CString::new("CPU_CLK_UNHALTED.CORE").unwrap(),    // Intel Yonah, Merom
+            ]
+        }
+        Event::Instructions => {
+            vec![
+                CString::new("FIXED_INSTRUCTIONS").unwrap(),  // Apple A7-A15
+                CString::new("INST_RETIRED.ANY").unwrap(),    // Intel Yonah, Merom, Core 1th-10th
+            ]
+        }
+        Event::Branches => {
+            vec![
+                CString::new("INST_BRANCH").unwrap(),             // Apple A7-A15
+                CString::new("BR_INST_RETIRED.ALL_BRANCHES").unwrap(),  // Intel Core 1th-10th
+                CString::new("INST_RETIRED.ANY").unwrap(),    // Intel Yonah, Merom
+            ]
+        }
+        Event::BranchMisses => {
+            vec![
+                CString::new("BRANCH_MISPRED_NONSPEC").unwrap(),       // Apple A7-A15, since iOS 15, macOS 12
+                CString::new("BRANCH_MISPREDICT").unwrap(),            // Apple A7-A14
+                CString::new("BR_MISP_RETIRED.ALL_BRANCHES").unwrap(), // Intel Core 2th-10th
+                CString::new("BR_INST_RETIRED.MISPRED").unwrap(),      // Intel Yonah, Merom
+            ]
+        }
+    }
+}
+
+pub fn get_event(event_type: Event, db: &mut kpep_db) -> Option<*mut kpep_event>{
+    let names = get_event_names(event_type);
+    for name in names {
+        unsafe {
+            let mut ev: *mut kpep_event = null_mut();
+            if kpep_db_event(db, name.as_ptr(), &mut ev) == 0 {
+                return Some(ev);
+            }
+        }
+    }
+    return None;
+}
+
+pub fn check_kpc_permission() -> Result<(), KperfError>{
+    let mut force_ctrs: c_int = 0;
+    unsafe {
+        let res = kperf_sys::functions::kpc_force_all_ctrs_get(&mut force_ctrs);
+        if res != 0 {
+            println!("Permission denied, xnu/kpc requires root privileges (error code: {})", res);
+            return Err(KperfError::PermissionDenied)
+        }
+    }
+    return Ok(())
 }
--- kperf-tests/src/main.rs
+++ kperf-tests/src/main.rs
@@ -2,99 +2,31 @@ use std::ffi::{CStr, CString};
 use std::mem::size_of;
 use std::ptr::{null, null_mut};
 use libc::{c_char, c_int, c_uint, c_ulonglong, size_t};
-use kperf_sys;
-use kperf_sys::constants::{KPC_CLASS_CONFIGURABLE, KPC_CLASS_CONFIGURABLE_MASK};
-use kperf_sys::functions::{kpc_force_all_ctrs_get, kpc_force_all_ctrs_set, kpc_get_thread_counters, kpc_set_config, kpc_set_counting, kpc_set_thread_counting, kpep_config_add_event, kpep_config_create, kpep_config_force_counters, kpep_config_kpc, kpep_config_kpc_classes, kpep_config_kpc_count, kpep_config_kpc_map, kpep_db_event};
-use kperf_sys::structs::{kpc_config_t, kpep_db, kpep_event};
+use kperf_rs::{kperf_sys, KProbesConfig, KProbesDatabase};
+use kperf_rs::kperf_sys::constants::{KPC_CLASS_CONFIGURABLE, KPC_CLASS_CONFIGURABLE_MASK};
+use kperf_rs::kperf_sys::functions::{kpc_force_all_ctrs_get, kpc_force_all_ctrs_set, kpc_get_thread_counters, kpc_set_config, kpc_set_counting, kpc_set_thread_counting, kpep_config_add_event, kpep_config_create, kpep_config_force_counters, kpep_config_kpc, kpep_config_kpc_classes, kpep_config_kpc_count, kpep_config_kpc_map, kpep_db_event};
+use kperf_rs::kperf_sys::structs::{kpc_config_t, kpep_db, kpep_event};
+use kperf_rs;
+use kperf_rs::{check_kpc_permission, Event, get_event};
 
 const KPC_MAX_COUNTERS: size_t = 32;
 
-#[derive(Copy, Clone, Debug)]
-pub enum Event {
-    Cycles,
-    Instructions,
-    Branches,
-    BranchMisses,
-}
-
-pub fn get_event_names(event_type: Event) -> Vec<CString> {
-    match event_type {
-        Event::Cycles => {
-            vec![
-                CString::new("FIXED_CYCLES").unwrap(),             // Apple A7-A15
-                CString::new("CPU_CLK_UNHALTED.THREAD").unwrap(),  // Intel Core 1th-10th
-                CString::new("CPU_CLK_UNHALTED.CORE").unwrap(),    // Intel Yonah, Merom
-            ]
-        }
-        Event::Instructions => {
-            vec![
-                CString::new("FIXED_INSTRUCTIONS").unwrap(),  // Apple A7-A15
-                CString::new("INST_RETIRED.ANY").unwrap(),    // Intel Yonah, Merom, Core 1th-10th
-            ]
-        }
-        Event::Branches => {
-            vec![
-                CString::new("INST_BRANCH").unwrap(),             // Apple A7-A15
-                CString::new("BR_INST_RETIRED.ALL_BRANCHES").unwrap(),  // Intel Core 1th-10th
-                CString::new("INST_RETIRED.ANY").unwrap(),    // Intel Yonah, Merom
-            ]
-        }
-        Event::BranchMisses => {
-            vec![
-                CString::new("BRANCH_MISPRED_NONSPEC").unwrap(),       // Apple A7-A15, since iOS 15, macOS 12
-                CString::new("BRANCH_MISPREDICT").unwrap(),            // Apple A7-A14
-                CString::new("BR_MISP_RETIRED.ALL_BRANCHES").unwrap(), // Intel Core 2th-10th
-                CString::new("BR_INST_RETIRED.MISPRED").unwrap(),      // Intel Yonah, Merom
-            ]
-        }
-    }
-}
-
-pub fn get_event(event_type: Event, db: &mut kpep_db) -> Option<*mut kpep_event>{
-    let names = get_event_names(event_type);
-    for name in names {
-        unsafe {
-            let mut ev: *mut kpep_event = null_mut();
-            if kpep_db_event(db, name.as_ptr(), &mut ev) == 0 {
-                return Some(ev);
-            }
-        }
-    }
-    return None;
-}
-
-
-
 fn main() {
-    let mut force_ctrs: c_int = 0;
+    // Check permission
+    check_kpc_permission().expect("KPC Permission denied");
+
+    let mut db = KProbesDatabase::load_database().expect("Couldn't load KProbes database");
+    // Load pmc database
+
+    // TODO: Make those functions of KProbesDatabase
+    // let db_name = CStr::from_ptr((*db).name);
+    // let db_marketing_name = CStr::from_ptr((*db).marketing_name);
+    // println!("loaded db: {:?} ({:?})", db_name, db_marketing_name);
+
+    let config = KProbesConfig::from_database(&mut db).expect("Failed to create kpep config");
+
     unsafe {
-        // Check permission
-        if kperf_sys::functions::kpc_force_all_ctrs_get(&mut force_ctrs) != 0 {
-            println!("Permission denied, xnu/kpc requires root privileges");
-            return;
-        }
-        println!("{}", force_ctrs);
-
-        // Load pmc database
-        let mut db: *mut kpep_db = null_mut();
-        let mut ret = kperf_sys::functions::kpep_db_create(
-            null(),
-            &mut db,
-        );
-        let db_name = CStr::from_ptr((*db).name);
-        let db_marketing_name = CStr::from_ptr((*db).marketing_name);
-        println!("loaded db: {:?} ({:?})", db_name, db_marketing_name);
-
-        let mut config = null_mut();
-        let res = kpep_config_create(db, &mut config);
-        if res != 0 {
-            println!(
-                "Failed to create kpep config, error: {}",
-                res
-            )
-        }
-
-        let mut res = kpep_config_force_counters(config);
+        let mut res = kpep_config_force_counters(config.config);
         if res != 0 {
             println!(
                 "Failed to force counters, error: {}",
@@ -102,14 +34,14 @@ fn main() {
             )
         }
 
-        let mut ev_branches = get_event(Event::Branches, &mut *db).unwrap();
-        kpep_config_add_event(config, &mut ev_branches, 0, null_mut());
-        let mut ev_cycles = get_event(Event::Cycles, &mut *db).unwrap();
-        kpep_config_add_event(config, &mut ev_cycles, 0, null_mut());
+        let mut ev_branches = get_event(Event::Branches, &mut *(db.database)).unwrap();
+        kpep_config_add_event(config.config, &mut ev_branches, 0, null_mut());
+        let mut ev_cycles = get_event(Event::Cycles, &mut *(db.database)).unwrap();
+        kpep_config_add_event(config.config, &mut ev_cycles, 0, null_mut());
 
         let mut classes = 0;
         let mut reg_count = 0;
-        let res = kpep_config_kpc_classes(config, &mut classes);
+        let res = kpep_config_kpc_classes(config.config, &mut classes);
         if res != 0 {
             println!(
                 "Failed to get kpc classes, error: {}",
@@ -120,7 +52,7 @@ fn main() {
             "kpc classes: {}",
             classes
         );
-        let res = kpep_config_kpc_count(config, &mut reg_count);
+        let res = kpep_config_kpc_count(config.config, &mut reg_count);
         if res != 0 {
             println!(
                 "Failed to get kpc count, error: {}",
@@ -136,7 +68,7 @@ fn main() {
             "{}",
             size_of::<[size_t; KPC_MAX_COUNTERS]>()
         );
-        let res = kpep_config_kpc_map(config, counter_map.as_mut_ptr(), size_of::<[size_t; KPC_MAX_COUNTERS]>());
+        let res = kpep_config_kpc_map(config.config, counter_map.as_mut_ptr(), size_of::<[size_t; KPC_MAX_COUNTERS]>());
         if res != 0 {
             println!(
                 "Failed to initialize kpc map, error: {}",
@@ -149,7 +81,7 @@ fn main() {
         );
 
         let mut regs: [kpc_config_t; KPC_MAX_COUNTERS] = [0; KPC_MAX_COUNTERS];
-        let res = kpep_config_kpc(config, regs.as_mut_ptr(), size_of::<[kpc_config_t; KPC_MAX_COUNTERS]>());
+        let res = kpep_config_kpc(config.config, regs.as_mut_ptr(), size_of::<[kpc_config_t; KPC_MAX_COUNTERS]>());
         if res != 0 {
             println!(
                 "Failed to configure kpc, error: {}",
